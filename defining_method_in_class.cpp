7b9ddbf620e17647a8b14c8c9fcedb4705067190
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-08-29 11:37:38 +0530

    Create defining_method_in_class.cpp

---
+++ defining_method_in_class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    void Mymethod(){
+        cout<<"I am studying in 2nd year!";
+    } 
+};student s1;
+int main(){
+    s1.Mymethod();
+    return 0;
+}
+
+/*
+I am studying in 2nd year!
+*/
